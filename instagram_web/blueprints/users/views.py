3f4128c4e80c2d5826180d8ce3e689514a5f3774
Lee <l783l74@hotmail.com>
2019-11-07 16:57:45 +0800

    Add new user successful

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,38 +1,47 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-
+from models.user import User
 
 users_blueprint = Blueprint('users',
                             __name__,
                             template_folder='templates')
 
+"""""""""""""""""""""""""""""""""
+Others
+"""""""""""""""""""""""""""""""""
+def error_to_flash(errors):
+    for error in errors:
+        flash(error, 'alert alert-danger')
+
+"""""""""""""""""""""""""""""""""
+Route functions
+"""""""""""""""""""""""""""""""""
 @users_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('users/new.html')
-
 
 @users_blueprint.route('/', methods=['POST'])
 def create():
     username = request.form.get('username')
     email = request.form.get('email')
-    password = request.form.get('password')
-    print(f"username: {username}, email: {email}, password: {password}")
-    return render_template('users/new.html')
-
+    password = request.form.get('password')  
+    user = User(username=username, email=email, password=password)
+    user.save()  
+    string = f"username: {username}, email: {email}, password: {password}"
+    print(string)
+    flash(string, 'alert alert-primary')
+    return redirect(url_for('users.new'))
 
 @users_blueprint.route('/<username>', methods=["GET"])
 def show(username):
     pass
 
-
 @users_blueprint.route('/', methods=["GET"])
 def index():
     return "USERS"
 
-
 @users_blueprint.route('/<id>/edit', methods=['GET'])
 def edit(id):
     pass
-
 
 @users_blueprint.route('/<id>', methods=['POST'])
 def update(id):
--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
 from models.base_model import BaseModel
 import peewee as pw
 
-
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True)
+    email = pw.CharField(unique=False)
+    password = pw.CharField(unique=False)
+
+    def validate(self):
+        print("user validate")
